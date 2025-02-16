dd27beec7a21910b985ca7632ce93ab82c9c5fae
FelipeKondoNakano <nakano.felipe02@gmail.com>
2025-02-16 12:10:24 -0300

    Atividades realizadas de laboratório de Engenharia de Software

---
+++ Atividade_1/script.js
@@ -0,0 +1,8 @@
+function elementParImpar(){
+    let number = document.getElementById("number").value
+    if(isNaN(number) || number < 0){
+        alert("Digite um número inteiro válido!");
+    }
+    else
+        number % 2 == 0 ? alert("É par!") : alert("É ímpar!");
+}
